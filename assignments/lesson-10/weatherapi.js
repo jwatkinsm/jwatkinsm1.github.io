0f3686acc2958bbf5c15143677bfe1c29a0cb1fc
jwatkinsm <wat18023@byui.edu>
2019-06-27 17:57:00 -0400

    api activity start

---
+++ assignments/lesson-10/weatherapi.js
@@ -0,0 +1,10 @@
+var weatherRequest= new XMLHttpRequest();
+var apiURLstring= 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=1446b3571d31230010c918221292fee5';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload= function(){
+    var weatherData= JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    document.getElementById('cc-temp').innerhtml= weatherData.main.temp;
+}
