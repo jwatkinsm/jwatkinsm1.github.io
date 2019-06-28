5905b5bf2480a1c37d6555df2dbc8a29ad08a224
jwatkinsm <wat18023@byui.edu>
2019-06-28 18:23:28 -0400

    start and summary

---
+++ assignments/lesson-10/script/towninfo.js
@@ -0,0 +1,53 @@
+var section = document.querySelector('.townData');
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    var townData = request.response;
+    var townArray = townData['towns'];
+
+    for (var i = 0; i < townArray.length; i++) {
+        if (townArray[i].name == 'Fish Haven' || townArray[i].name == 'Preston' || townArray[i].name == 'Soda Springs') {
+
+            var article = document.createElement('article')
+            var h3 = document.createElement('h3');
+            var para1 = document.createElement('p');
+            var para2 = document.createElement('p');
+            var para3 = document.createElement('p');
+            var para4 = document.createElement('p');
+            var image = document.createElement("img");
+
+            h3.textContent = townArray[i].name;
+            para1.textContent = townArray[i].motto;
+            para2.textContent = "Year Founded:  " + townArray[i].yearFounded;
+            para3.textContent = "Population:  " + townArray[i].currentPopulation;
+            para4.textContent = "Average Rainfall:  " + townArray[i].averageRainfall + " in";
+
+            
+           
+            
+            if (townArray[i].name == 'Fish Haven') {
+                image.setAttribute("src", "images/fishhaventown.jpg");
+                image.setAttribute("alt", "Lakeside ");
+            } else if (townArray[i].name == 'Preston') {
+                image.setAttribute("src", "images/prestontown.jpg");
+                image.setAttribute("alt", "Town sign");
+            } else if (townArray[i].name == 'Soda Springs') {
+                image.setAttribute("src", "images/sodaspringstown.jpg");
+                image.setAttribute("alt", "Geyser ");
+            }
+            article.appendChild(h3);
+            article.appendChild(para1);
+            article.appendChild(para2);
+           article.appendChild(para3);
+            article.appendChild(para4);
+            article.appendChild(image);
+            
+            section.appendChild(article);
+        }
+    }
+
+}
--- assignments/lesson-10/weatherapi.js
+++ assignments/lesson-10/weatherapi.js
@@ -6,5 +6,31 @@ weatherRequest.send();
 weatherRequest.onload= function(){
     var weatherData= JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
-    document.getElementById('temp').innerHTML= weatherData.main.temp;
+    document.getElementById('temp').innerHTML = weatherData.main.temp;
+    document.getElementById('current').innerHTML = weatherData.weather[0].description;
+    document.getElementById('humidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('windspeed').innerHTML = weatherData.wind.speed;
+
+    /*wind chill*/
+    var t = parseInt(document.getElementById('temp').innerHTML);
+    var s = parseInt(document.getElementById('windspeed').innerHTML);
+    var f = 35.74 + (0.6215*t) - (35.75*Math.pow(s, 0.16))+(0.4275*t*Math.pow(s, 0.16));
+    var result = f.toFixed(0)
+    document.getElementById('windchill').innerHTML= result;
+}
+/*forecat*/
+var forecastRequest= new XMLHttpRequest();
+var apiURLstring= 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=1446b3571d31230010c918221292fee5';
+forecastRequest.open('Get', apiURLstring, true);
+forecastRequest.send();
+
+var lsits= [];
+forecastRequest.onload= function(){
+    var forecastData= JSON.parse(forecastRequest.responseText);
+    console.log(forecastData);
+    for (i= 0; f< forecastData.list.length; i++){
+        if (forecastData.lsit[i].dt_txt.includes("18:00:00"));
+        console.log(forecastData.list[i]);
+    }
+    document.getElementById('day1').innerHTML= lists[0].main.temp;
 }
