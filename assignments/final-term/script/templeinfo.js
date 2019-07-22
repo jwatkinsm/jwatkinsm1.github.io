94dae7323bd0e89f85f3d2996bc93ef80c441dba
jwatkinsm <wat18023@byui.edu>
2019-07-22 17:05:44 -0400

    rename

--- assignments/final-term/script/templeinfo.js
+++ assignments/final-term/script/templeinfo.js
@@ -1,5 +1,5 @@
 var div = document.querySelector('div');
-var requestURL = 'https://jwatkinsm.github.io/assignments/final term/script/temples.json';
+var requestURL = 'https://jwatkinsm.github.io/assignments/final-term/script/temples.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
